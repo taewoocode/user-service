11cc0c5ee3487fdb3d1eb1c3210cff261676a071
taewoocode <qkrxodn6035@gmail.com>
2025-07-03 21:56:28 +0900

    US-1[feat]: 프로필 상세조회시 조회수 1

--- src/main/java/com/example/user_service/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/example/user_service/user/repository/UserRepositoryCustom.java
@@ -3,9 +3,12 @@ package com.example.user_service.user.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.example.user_service.user.dto.UserProfileDetailInfo;
 import com.example.user_service.user.dto.UserProfileInfo;
 
 public interface UserRepositoryCustom {
 	Page<UserProfileInfo.UserProfileResponse> searchProfiles(UserProfileInfo.UserProfileRequest request,
 		Pageable pageable);
+
+	UserProfileDetailInfo.UserProfileDetailResponse findAndIncreaseViewCount(Long userId);
 }
