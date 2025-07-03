d919a0c6311167ef89fb7e0778dbdb892a54aa7b
taewoocode <qkrxodn6035@gmail.com>
2025-07-03 21:30:49 +0900

    US-1[feat]: UserRepository 회원 프로필 조회 추가

---
+++ src/main/java/com/example/user_service/user/repository/UserRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.example.user_service.user.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.user_service.user.dto.UserProfileInfo;
+
+public interface UserRepositoryCustom {
+	Page<UserProfileInfo.UserProfileResponse> searchProfiles(UserProfileInfo.UserProfileRequest request,
+		Pageable pageable);
+}
