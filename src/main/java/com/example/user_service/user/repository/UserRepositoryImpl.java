385ae771f8cc6db934276950b2c8d88452398c9c
taewoocode <qkrxodn6035@gmail.com>
2025-07-03 21:56:48 +0900

    US-1[feat]: 프로필 상세조회시 조회수 카운팅

--- src/main/java/com/example/user_service/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/user_service/user/repository/UserRepositoryImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.example.user_service.user.domain.QUser;
+import com.example.user_service.user.dto.UserProfileDetailInfo;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
@@ -55,6 +56,33 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 		long total = queryFactory.selectFrom(user).where(builder).fetchCount();
 
 		return new PageImpl<>(content, pageable, total);
+	}
+
+	@Override
+	public UserProfileDetailInfo.UserProfileDetailResponse findAndIncreaseViewCount(Long userId) {
+		QUser user = QUser.user;
+		long updateRows = queryFactory.update(user)
+			.set(user.viewCount, user.viewCount.add(1))
+			.where(user.id.eq(userId))
+			.execute();
+
+		if (updateRows == 0) {
+			throw new IllegalArgumentException("유저를 찾을 수 없습니다.");
+		}
+
+		return queryFactory
+			.select(user)
+			.where(user.id.eq(userId))
+			.fetch()
+			.stream()
+			.findFirst()
+			.map(u -> UserProfileDetailInfo.UserProfileDetailResponse.builder()
+				.userId(u.getId())
+				.name(u.getName())
+				.viewCount(u.getViewCount())
+				.createdAt(u.getCreatedAt())
+				.build()).orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
 	}
 
 	private OrderSpecifier<?> getOrderSpecifier(String sortBy, String direction, QUser user) {
