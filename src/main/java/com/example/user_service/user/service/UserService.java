93e2a10d22101ecd4c7e1feb166a884d2af7502a
taewoocode <qkrxodn6035@gmail.com>
2025-07-04 20:52:07 +0900

    US-2[feat]: import 정렬

--- src/main/java/com/example/user_service/user/service/UserService.java
+++ src/main/java/com/example/user_service/user/service/UserService.java
@@ -1,13 +1,15 @@
 package com.example.user_service.user.service;
 
-import com.example.user_service.user.dto.UserRegisterInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
 import com.example.user_service.user.dto.UserProfileInfo;
+import com.example.user_service.user.dto.UserRegisterInfo;
 
 public interface UserService {
 
 	UserRegisterInfo.UserRegisterResponse saveUser(UserRegisterInfo.UserRegisterRequest request);
 
-	Page<UserProfileInfo.UserProfileResponse> searchProfiles(UserProfileInfo.UserProfileRequest request, Pageable pageable);
+	Page<UserProfileInfo.UserProfileResponse> searchProfiles(UserProfileInfo.UserProfileRequest request,
+		Pageable pageable);
 }
