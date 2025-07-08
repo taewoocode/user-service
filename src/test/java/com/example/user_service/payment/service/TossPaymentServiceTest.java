80b0f9b3fdd1531539b024386c8835f6bb8b7544
taewoocode <qkrxodn6035@gmail.com>
2025-07-08 15:32:14 +0900

    US-3[feat]: 결제 요청 생성 메서드 분리

--- src/test/java/com/example/user_service/payment/service/TossPaymentServiceTest.java
+++ src/test/java/com/example/user_service/payment/service/TossPaymentServiceTest.java
@@ -54,35 +54,15 @@ class TossPaymentServiceTest {
 		// given
 		Long userId = 1L;
 		String paymentKey = "TOSS_TEST_KEY";
-
-		PaymentPrepareRequest request = PaymentPrepareRequest.builder()
-			.userId(userId)
-			.amount(10000)
-			.paymentMethod("CARD")
-			.orderId("ORDER_123")
-			.successUrl("https://test.com/success")
-			.failUrl("https://test.com/fail")
-			.build();
-
+		PaymentPrepareRequest request = createPaymentRequest(userId);
 		User user = new User("taewoo", "taewoo", "1234", 0);
 		Payment mockPayment = mock(Payment.class);
-
-		PaymentPrepareResponse mockResponse = PaymentPrepareResponse.builder()
-			.paymentKey(paymentKey)
-			.paymentUrl("https://toss.payments.com/redirect")
-			.orderId("ORDER_123")
-			.expiredAt(LocalDateTime.now().plusMinutes(10))
-			.paymentStatus("READY")
-			.status("SUCCESS")
-			.build();
+		PaymentPrepareResponse mockResponse = createPaymentResponse(paymentKey);
 
 		when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-
 		when(tossPaymentProperties.getBaseUrl()).thenReturn("https://toss.payments.com");
-
 		when(tossApiClient.requestPayment(any(), any())).thenReturn(mockResponse);
 
-		// mock: 정적 메서드
 		try (MockedStatic<PaymentConverter> converterMock = mockStatic(PaymentConverter.class)) {
 			converterMock.when(() -> PaymentConverter.toPayment(eq(request), anyString(), eq(user)))
 				.thenReturn(mockPayment);
@@ -102,5 +82,29 @@ class TossPaymentServiceTest {
 			verify(paymentRepository).save(mockPayment);
 			verify(tossApiClient).requestPayment(any(), any());
 		}
+	}
+
+	private static PaymentPrepareResponse createPaymentResponse(String paymentKey) {
+		PaymentPrepareResponse mockResponse = PaymentPrepareResponse.builder()
+			.paymentKey(paymentKey)
+			.paymentUrl("https://toss.payments.com/redirect")
+			.orderId("ORDER_123")
+			.expiredAt(LocalDateTime.now().plusMinutes(10))
+			.paymentStatus("READY")
+			.status("SUCCESS")
+			.build();
+		return mockResponse;
+	}
+
+	private static PaymentPrepareRequest createPaymentRequest(Long userId) {
+		PaymentPrepareRequest request = PaymentPrepareRequest.builder()
+			.userId(userId)
+			.amount(10000)
+			.paymentMethod("CARD")
+			.orderId("ORDER_123")
+			.successUrl("https://test.com/success")
+			.failUrl("https://test.com/fail")
+			.build();
+		return request;
 	}
 }
