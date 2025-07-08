21a70d08a776609f9881a7c50d660d39d6dad0db
taewoocode <qkrxodn6035@gmail.com>
2025-07-08 16:41:53 +0900

    US-3[feat]: 쿠폰 엔티티 추가

---
+++ src/main/java/com/example/user_service/payment/domain/Coupon.java
@@ -0,0 +1,33 @@
+package com.example.user_service.payment.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "coupon")
+public class Coupon {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String code;
+
+	@Column(nullable = false)
+	private double discountRate; // 0.2 = 20%
+
+	@Column(nullable = false)
+	private long maxDiscount; // ex) 5000
+
+	@Column(nullable = false)
+	private LocalDateTime expiredAt;
+
+	@Column(nullable = false)
+	private boolean enabled;
+}
